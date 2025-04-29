3e64eb7bce52b3832ef9ef0b1b0c694ff37c6508
wanzofc only <zanssxploit@gmail.com>
2025-04-29 18:47:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
+
 const app = express();
 app.use(express.json());
+const currentDirectory = path.join(__dirname);
+
 const API_KEY = 'jcj6uqsC';
 const API_URL = 'https://api.only-awan.biz.id/api/ai/gpt3';
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(currentDirectory, 'index.html'));
+});
+
 app.post('/api/chat', async (req, res) => {
   try {
     const userMessage = req.body.message;
     const combinedPrompt = `kamu adalah wanzofc, ${userMessage}`;
+
     const response = await axios.get(API_URL, {
       params: {
-        prompt: combinedPrompt, 
+        prompt: combinedPrompt,
         apikey: API_KEY
       }
     });
+
     res.send({ message: response.data.choices[0].text });
   } catch (error) {
     console.error(error);
     res.status(500).send({ error: error.message });
   }
 });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
